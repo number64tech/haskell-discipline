0cf5cf7c823e59fb2c5a4f5a3ee8ca88d71d1da5
TODOROKI Daisuke <number64tech@gmail.com>
2016-02-09 10:12:47 +0900

    Add new .hs file

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,6 @@
 module Main where
 
-import Lib
+import Coord
 
 main :: IO ()
 main = someFunc
---
+++ src/Coord.hs
@@ -0,0 +1,6 @@
+module Coord
+    ( someFunc
+    ) where
+
+someFunc :: IO ()
+someFunc = putStrLn "HelloHaskellWorld!!"
