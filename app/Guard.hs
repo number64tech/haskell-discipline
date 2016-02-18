69e8616ebcf9aa492a4d326de9686650d679531b
TODOROKI Daisuke <number64tech@gmail.com>
2016-02-18 16:09:29 +0900

    Use Debug.Trace for inline-debugging

--- app/Guard.hs
+++ app/Guard.hs
@@ -1,8 +1,13 @@
 module Guard where
 
+import Debug.Trace
+
 testFunc :: Int -> Int 
 testFunc 0 = 1  
 testFunc n | n > 0 = n * testFunc (n-1)
 
-main :: IO () 
-main = putStrLn (show (testFunc 15))
+input = 5
+result = trace ("calling testFunc with x = " ++ show input) (testFunc input)
+
+main :: IO ()
+main = putStrLn (show result)
